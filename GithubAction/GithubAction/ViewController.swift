3a1f2b9e8706b79b685e46a487f7612a53f9b8ad
wu_haotian <haotian_wu@thinkyeah.com>
2021-08-23 15:16:13 +0800

    IOS proj init.

---
+++ GithubAction/GithubAction/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GithubAction
+//
+//  Created by thinkyeah on 2021/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .brown
+    }
+
+
+}
+
